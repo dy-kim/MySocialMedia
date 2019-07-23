d73a191225f66ba14f25591346504454ceecb1da
Daeyoung Kim <daykim22@gmail.com>
2019-07-23 20:15:34 +0900

    Implement comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@
 
 class Article < ApplicationRecord
   has_many :comments
+  validates :title, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :articles
+
   resources :articles do
     resources :comments
   end
